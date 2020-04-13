96bad8797c3fcf36872e0f6eb9f3abff2d071dd9
Derrick Yirenkyi <21703340@bucks.ac.uk>
2020-04-13 21:50:05 +0100

    Add project files.

---
+++ courses/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="courses.MvcApplication" Language="C#" %>
---
+++ courses/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(courses.Startup))]
+namespace courses
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
