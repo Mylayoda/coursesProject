29a4b9aca28d215944f7d7dc614affc75ac69e4d
Derrick Yirenkyi <21703340@bucks.ac.uk>
2020-04-13 22:54:54 +0100

    js,layout

--- courses/App_Start/BundleConfig.cs
+++ courses/App_Start/BundleConfig.cs
@@ -20,11 +20,16 @@ namespace courses
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/Admin").Include(
+                     "~/Scripts/AdminMenu.js"));
+
         }
     }
 }
