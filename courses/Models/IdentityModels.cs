027683d951c765bb5d25227a3e9140c5a2055edd
Derrick Yirenkyi <21703340@bucks.ac.uk>
2020-04-14 07:12:51 +0100

    migration datatables added

--- courses/Models/IdentityModels.cs
+++ courses/Models/IdentityModels.cs
@@ -3,6 +3,8 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using courses.Entities;
 
 namespace courses.Models
 {
@@ -29,5 +31,7 @@ namespace courses.Models
         {
             return new ApplicationDbContext();
         }
+        public DbSet<Section> Sections { get; set; }
+        public DbSet<Part> Parts { get; set; }
     }
 }
